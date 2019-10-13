8406cca19dc3500f07d8c3da6d8647987eecec9b
qr-mipro <Kostychik97@gmail.com>
2019-10-14 00:51:29 +0300

    wrote new test for product page

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,17 @@
+import pytest
+import time
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
 
+@pytest.mark.skip()
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.skip()
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -14,3 +19,18 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+@pytest.mark.skip()
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_product_page()
+
+@pytest.mark.parametrize('link', ['0','1','2','3','4','5','6','7','8','9'])
+def test_guest_should_see_product_promo_offer_pages(browser, link):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + link
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_product_page()
+
