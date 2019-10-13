8e76fa22890eb747e2c38d6ae9dc69921c32c2e9
qr-mipro <Kostychik97@gmail.com>
2019-10-14 00:53:20 +0300

    did changes to additional pages and locator according new ProductObject class

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,39 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    # Login fields
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_SUBMIT = (By.CSS_SELECTOR, "[name=login_submit]")
+    # Registration fields
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "[name=registration_submit]") 
+
+class ProductPageLocators():
+    # Forms
+    PRODUCT_FORM = (By.CSS_SELECTOR, ".product_page") 
+    REVIEW_FORM = (By.CSS_SELECTOR, "#addreview") 
+    # Product fields
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1") 
+    PRODUCT_COST = (By.CSS_SELECTOR, ".product_main .price_color") 
+    PRODUCT_AVAILABILITY = (By.CSS_SELECTOR, ".product_main .availability") 
+    PRODUCT_INSTOCK = (By.CSS_SELECTOR, ".product_main .instock") 
+    BASKET_NAME = (By.CSS_SELECTOR, "#messages .alert-success strong")
+    BASKET_COST = (By.CSS_SELECTOR, "#messages .alert-info strong")
+    # Buttons
+    BUTTON_ADD_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket") 
+    BUTTON_WRITE_REVIEW = (By.CSS_SELECTOR, "#write_review") 
+    # TODO: Review fields
+    REVIEW_TITLE = (By.CSS_SELECTOR, "#id_title") 
+    REVIEW_SCORE = (By.CSS_SELECTOR, "#id_score") 
+    REVIEW_BODY= (By.CSS_SELECTOR, "#id_body") 
+    REVIEW_NAME= (By.CSS_SELECTOR, "#id_name") 
+    REVIEW_EMAIL = (By.CSS_SELECTOR, "#id_email") 
+
+  
