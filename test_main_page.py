6f9827e348368f01ace13255b7b7d6d2a1abad03
qr-mipro <Kostychik97@gmail.com>
2019-10-14 23:02:48 +0300

    added negative tests

--- test_main_page.py
+++ test_main_page.py
@@ -27,6 +27,7 @@ def test_guest_can_add_product_to_basket(browser):
     product_page.open()
     product_page.should_be_product_page()
 
+@pytest.mark.skip()
 @pytest.mark.parametrize('link', ['0','1','2','3','4','5','6',
                                     pytest.param('7', marks = pytest.mark.xfail),
                                     '8','9'])
@@ -35,4 +36,26 @@ def test_guest_should_see_product_promo_offer_pages(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_add_product_to_basket()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_not_be_success_message()
+
+def test_guest_cant_see_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.should_message_disappear()
 
